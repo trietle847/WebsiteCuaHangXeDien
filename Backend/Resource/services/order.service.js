86abf1a5456cb391fb24642a348cb40c247c9e4d
Triết Lê <trietle847@example.com>
2025-09-30 07:58:07 +0700

    add order service

---
+++ Backend/Resource/services/order.service.js
@@ -0,0 +1,90 @@
+const OrderModel = require("../models/order.model");
+const OrderDetail = require("../models/orderDetail.model");
+const ProductModel = require("../models/product.model");
+const { sequelize } = require("../utils/db");
+
+class OrderService {
+  async createOrder(userId, products) {
+    const transaction = await sequelize.transaction();
+
+    try {
+      // Lấy danh sách product_id
+      const productIds = products.map((p) => p.product_id);
+
+      // Lấy thông tin sản phẩm từ DB
+      const dbProducts = await ProductModel.findAll({
+        where: { product_id: productIds },
+        transaction,
+        lock: transaction.LOCK.UPDATE,
+      });
+
+      const productPriceMap = {};
+      dbProducts.forEach((prod) => {
+        productPriceMap[prod.product_id] = prod.price;
+      });
+
+      const totalAmount = products.reduce((sum, product) => {
+        const price = productPriceMap[product.product_id];
+        return sum + price * product.quantity;
+      }, 0);
+
+      // Tạo order
+      const order = await OrderModel.create(
+        {
+          user_id: userId,
+          payment_status: false,
+          delivery_status: false,
+          totalAmount,
+        },
+        { transaction }
+      );
+
+      // tạo thành công giảm lại số lượng trong kho
+      for (const product of products) {
+        await ProductModel.update(
+          {
+            stock_quantity: sequelize.literal(
+              `stock_quantity - ${product.quantity}`
+            ),
+          },
+          {
+            where: { product_id: product.product_id },
+            transaction,
+          }
+        );
+      }
+
+      const orderDetails = products.map((prod) => ({
+        order_id: order.order_id,
+        product_id: prod.product_id,
+        quantity: prod.quantity,
+        // price: productPriceMap[prod.product_id], // giá lúc đặt hàng
+      }));
+
+      await OrderDetail.bulkCreate(orderDetails, { transaction });
+
+      await transaction.commit();
+
+      return { order, orderDetails };
+    } catch (error) {
+      await transaction.rollback();
+      throw new Error("Lỗi khi tạo đơn hàng: " + error.message);
+    }
+  }
+
+  async getOrderByAdmin() {
+    const orders = await OrderModel.findAll();
+
+    return orders;
+  }
+
+  async getOrderByUser(userId) {
+    const orders = await OrderModel.findAll({
+      where: {user_id: userId}
+    });
+
+    return orders
+  }
+}
+
+module.exports = new OrderService();
