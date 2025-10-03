7d3e87c0c3fbe5e52d31f9cf60870e03bf87cb84
Limito9x <limito9x@gmail.com>
2025-10-03 18:57:34 +0700

    usePolling for docker

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3001,
     open: false,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       "/api": {
         target:
