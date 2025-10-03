97055ce85612b70e0483b10d8678a86c744cf972
Triết Lê <trietle847@example.com>
2025-10-03 11:00:05 +0700

    cấu trúc lại thư mục

---
+++ Frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography} from "@mui/material";
+export default function Home() {
+    return (
+      <Box>
+        <Typography variant="h1" component="h1" gutterBottom>
+          Home Page
+        </Typography>
+      </Box>
+    );
+}
