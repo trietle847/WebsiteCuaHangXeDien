eeb52ebd8153d62e4e0d0d6cd8ab6c1f75e9dd97
Triết Lê <trietle847@example.com>
2025-10-25 10:33:33 +0700

    update

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,8 @@ const feedbackRoute = require("./Resource/routes/feedback.route");
 const orderController = require("./Resource/routes/order.route");
 const companyRoute = require("./Resource/routes/company.route");
 const colorRoute = require("./Resource/routes/color.route")
+const repairScheduleRoute = require("./Resource/routes/repairSchedule.route")
+const searchRoute = require("./Resource/routes/search.route")
 
 const app = express();
 
@@ -43,6 +45,9 @@ app.use("/comment", feedbackRoute);
 app.use("/order", orderController);
 app.use("/company", companyRoute);
 app.use("/color", colorRoute);
+app.use("/repair", repairScheduleRoute)
+
+app.use("/search", searchRoute)
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
