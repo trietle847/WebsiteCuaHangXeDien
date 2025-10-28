6ccb2ebb666402d85be5a441114b5d74219b8952
Limito9x <limito9x@gmail.com>
2025-10-28 15:12:50 +0700

    sửa lại kiểm soát quyền hạn

--- Backend/Resource/middlewares/auth.middleware.js
+++ Backend/Resource/middlewares/auth.middleware.js
@@ -1,41 +1,51 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "secret_key";
 
 function authMiddleware(req, res, next) {
-    const authHeader = req.headers["authorization"] || req.headers["Authorization"]
-    const token =
-      authHeader && authHeader.startsWith("Bearer ")
-        ? authHeader.split(" ")[1]
-        : null;
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+  const token =
+    authHeader && authHeader.startsWith("Bearer ")
+      ? authHeader.split(" ")[1]
+      : null;
 
-    if (!token) {
-        return res.status(401).json({message: "Chưa có token"})
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Chưa có token" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET)
-        req.user = decoded
-        next();
-        console.log(decoded);
-    } catch (error) {
-      console.error("Lỗi xác thực token:", error);
-        return res.status(403).json({ error: "Token không hợp lệ hoặc đã hết hạn" });
-    }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+    console.log(decoded);
+  } catch (error) {
+    console.error("Lỗi xác thực token:", error);
+    return res
+      .status(403)
+      .json({ error: "Token không hợp lệ hoặc đã hết hạn" });
+  }
 }
+
+const staffRoles = ["mechanic", "store_keeper", "sale_staff"];
+const isValidStaff = (role) => {
+  return staffRoles.includes(role);
+};
 
 function authorizeRoles(...allowedRoles) {
   return (req, res, next) => {
-    if (!req.user || !Array.isArray(req.user.roles)) {
-      return res.status(403).json({ error: "Không có quyền truy cập" });
+    if (!req.user) {
+      return res.status(403).json({ message: "Không có quyền truy cập" });
     }
-
-    const hasRole = req.user.roles.some((r) => allowedRoles.includes(r));
+    let hasRole = false;
+    if (allowedRoles.includes("staff")) {
+      hasRole = isValidStaff(req.user.role);
+    }
+    else hasRole = allowedRoles.includes(req.user.role);
     if (!hasRole) {
       return res
         .status(403)
-        .json({ error: "Bạn không có quyền thực hiện thao tác này" });
+        .json({ message: "Bạn không có quyền thực hiện thao tác này" });
     }
-
     next();
   };
 }
