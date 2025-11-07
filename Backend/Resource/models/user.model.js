00721a9d140fce38bb368c9ffccf5a2dd00e1e14
Limito9x <limito9x@gmail.com>
2025-11-07 21:41:35 +0700

    thêm createAt cho user phục vụ cho báo cáo người dùng

--- Backend/Resource/models/user.model.js
+++ Backend/Resource/models/user.model.js
@@ -5,11 +5,11 @@ const UserModel = sequelize.define(
   "User",
   {
     user_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    username: { type: DataTypes.STRING(100), allowNull: true }, // Tránh trùng lặp
+    username: { type: DataTypes.STRING(100), allowNull: true },
     password: { type: DataTypes.STRING(100), allowNull: true },
     first_name: { type: DataTypes.STRING(100), allowNull: true },
     last_name: { type: DataTypes.STRING(100), allowNull: true },
-    email: { type: DataTypes.STRING(100), allowNull: false }, // Đúng ra nên là unique: true nhưng để tạm vậy cho dễ test
+    email: { type: DataTypes.STRING(100), allowNull: false },
     phone: { type: DataTypes.STRING(100), allowNull: true },
     address: { type: DataTypes.STRING(255), allowNull: true },
     role: {
@@ -22,24 +22,25 @@ const UserModel = sequelize.define(
       ),
       defaultValue: "user",
     },
-
     google_id: { type: DataTypes.STRING(100), allowNull: true },
     login_type: {
       type: DataTypes.ENUM("local", "google"),
       defaultValue: "local",
     },
-
-    // Dành cho kích hoạt tài khoản và đặt lại mật khẩu
     status: {
       type: DataTypes.ENUM("active", "inactive", "banned"),
       defaultValue: "active",
     },
     token_hash: { type: DataTypes.STRING(255), allowNull: true },
     token_expires_at: { type: DataTypes.DATE, allowNull: true },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
   },
   {
     timestamps: true,
-    createdAt: false,
     updatedAt: false,
     tableName: "user",
     paranoid: true,
