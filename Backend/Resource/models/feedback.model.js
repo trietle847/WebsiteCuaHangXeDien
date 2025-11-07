8ed474afe3190e78f6e2bac4f4ef43cae2a8dd8e
Tên của bạn <email@example.com>
2025-11-07 21:17:29 +0700

    Huy_Nhap

--- Backend/Resource/models/feedback.model.js
+++ Backend/Resource/models/feedback.model.js
@@ -4,14 +4,15 @@ const { sequelize } = require("../utils/db");
 const FeedbackModel = sequelize.define(
   "Feedback",
   {
-    feedback_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    content: {type: DataTypes.TEXT("long")},
-    reply: {type: DataTypes.INTEGER}, 
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    content: { type: DataTypes.TEXT("long") },
+    reply: { type: DataTypes.INTEGER },
     // date: {type: DataTypes.DATE}
   },
   {
     tableName: "feedback",
     // timestamps: false,
+    timestamps: false, // ✅ thêm dòng này
   }
 );
 
